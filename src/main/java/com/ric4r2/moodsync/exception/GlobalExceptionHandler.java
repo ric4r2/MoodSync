03f91ba26d856befe8e043ebaae7d79667406f2c
Ricardo Cruz <99630790+ric4r2@users.noreply.github.com>
2026-02-10 23:01:54 -0600

    chore: project structure setup (#2)

---
+++ src/main/java/com/ric4r2/moodsync/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.ric4r2.moodsync.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {}
---
+++ src/main/java/com/ric4r2/moodsync/repository/TrackRepository.java
@@ -0,0 +1,6 @@
+package com.ric4r2.moodsync.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository {}
