18df26640b1a744613d8a93a2c1adb070cfefd06
Ricardo Cruz <ric4r2.0201@gmail.com>
2026-02-11 11:11:49 -0600

    feat: setup OAuth2 logic and spotify configuration

--- src/main/java/com/ric4r2/moodsync/service/SpotifyService.java
+++ src/main/java/com/ric4r2/moodsync/service/SpotifyService.java
@@ -1,6 +1,33 @@
 package com.ric4r2.moodsync.service;
 
 import org.springframework.stereotype.Service;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.requests.authorization_code.AuthorizationCodeRequest;
+import java.io.IOException;
+import org.apache.hc.core5.http.ParseException;
 
 @Service
-public class SpotifyService {}
+public class SpotifyService {
+
+	private final SpotifyApi spotifyApi;
+
+	public SpotifyService(SpotifyApi spotifyApi) {
+		this.spotifyApi = spotifyApi;
+	}
+
+
+	public String exchangeCode(String code) {
+		try {
+			AuthorizationCodeRequest request = spotifyApi.authorizationCode(code).build();
+			AuthorizationCodeCredentials credentials = request.execute();
+
+			spotifyApi.setAccessToken(credentials.getAccessToken());
+			spotifyApi.setRefreshToken(credentials.getRefreshToken());
+
+			return credentials.getAccessToken();
+		} catch (IOException | SpotifyWebApiException | ParseException e) {
+			return "Error exchanging code: " + e.getMessage();
+		}
+	}
+}
